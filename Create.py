348164999a0e36af7feaf634dbe59d050da4a922
littleLemming <lemming0061@gmail.com>
2015-07-23 23:40:26 +0200

    dont know. maybe. with some missing things? ? ? ?

--- Create.py
+++ Create.py
@@ -2,16 +2,58 @@ __author__ = 'twi'
 
 class Create:
 
-    def __init__(self):
-        self.sudoku = ["         ","         ","         ","         ","         ","         ","         ","         ","         "]
+    #    --> y
+    #    _______
+    #   |       |    |
+    #   |       |    |
+    #   |       |    V
+    #    -------
+    #                x
+    #
+    # just. yes. that's how it works. so i know. =D
+
+    # writeToFile
+    #   if True, the sudokus will be safed
+    # file
+    #   file in that will be written if writeToFile is true
+    def __init__(self,writeToFile,file):
         self.sudokus = []
+        self.writeToFile = writeToFile
+        self.file = file
 
 
     # creates every possible sudoku. like. those are many. maybe they get written in a file or something.
-    def createAll(self):
+    # notAllowed
+    #   is an array-array that contains an empty string for each position in the sudoku
+    #   if something is inserted the lines and blocks in contact will be updated so that it is not possible to
+    #   put in a wrong number
+    # sudoku
+    #   the sudoku right now - as input: ["         ","         ","         ","         ","         ","         ","         ","         ","         "] please..
+    # x
+    #   int between 0 and 8, has to be 0 for the first call
+    # y
+    #   int between 0 and 8, has to be 0 for the first call
+    # num
+    # int between 1 and 9 - in the beginning 1
+    def createAll(self,x,y,notAllowed,sudoku,num):
+        if x < 0 or x > 8 or y < 0 or y > 8 or num < 1 or num > 9:
+            return
+        if x == 8 and y == 8:
+            self.sudokus.append(sudoku)
+        if num < 9:
+            sudokuCopy = sudoku[:]
+            notAllowedCopy = notAllowed[:]
+            self.createAll(x,y,notAllowedCopy,sudokuCopy,num+1)
+        sudokuCopy = sudoku[:]
+        notAllowedCopy = notAllowed[:]
 
-        return
+        # TODO: UPDATE notAllowed
 
+        sudokuCopy[x][y] = num
+        if y < 8:
+            self.createAll(x,y+1,notAllowedCopy,sudokuCopy,num)
+        else:
+            self.createAll(x+1,y,notAllowedCopy,sudokuCopy,num)
     def sudokuExists(self,sudoku):
         for i in self.sudokus:
             if self.equals(i,sudoku):
