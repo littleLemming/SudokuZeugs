5cc0a448989e1d144afea5cef174c4f2ba51b6f4
littleLemming <lemming0061@gmail.com>
2015-07-23 12:51:48 +0200

    i dont know

--- Create.py
+++ Create.py
@@ -4,13 +4,27 @@ class Create:
 
     def __init__(self):
         self.sudoku = ["         ","         ","         ","         ","         ","         ","         ","         ","         "]
+        self.sudokus = []
 
-
-    # creates num many sudokus - randomly. yaaaay. so. every time another one. except the random in python is shit.
-    # don't know. is it?
-    def createRandom(self,num):
-        return
 
     # creates every possible sudoku. like. those are many. maybe they get written in a file or something.
     def createAll(self):
+
         return
+
+    def sudokuExists(self,sudoku):
+        for i in self.sudokus:
+            if self.equals(i,sudoku):
+                return True
+        return False
+
+    # s1 and s2 have to be completed sudokus - they do not have to be correct though
+    # if they are completely the same, True, else False is
+    def equals(self,s1,s2):
+        x = 0
+        y = 0
+        while x < 9:
+            while y < 9:
+
+                y += 1
+            x += 1
