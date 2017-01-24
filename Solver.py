0926387dd59d4984a37d812ef87c3388e453317b
littleLemming <lemming0061@gmail.com>
2017-01-24 23:57:47 +0100

    restraining permutations

--- Solver.py
+++ Solver.py
@@ -2,22 +2,21 @@ import sys
 
 class Solver:
 
-    # solves regulat 9x9 sudokus
+    # solves regular 9x9 sudokus
     # input as array-array, for empty spaces 0
 
     def __init__(self, sudoku):
-        self.sudoku = sudoku
-        self.valid(self.sudoku)
+        self.valid(sudoku)
         self.solutions = []
 
-    def finished(self,sudoku):
+    def finished_0(self, sudoku):
         for row in sudoku:
             row_tmp = list(filter((0).__ne__, sudoku[i]))
                 if len(row_tmp) != len(set(row_tmp)):
                     return False
         return True
 
-    def valid(self, sudoku):
+    def valid_0(self, sudoku):
         if len(sudoku) != 9:
             sys.exit("invalid sudoku: invalid size of sudoku")
         for i in range (0,9):
@@ -41,6 +40,9 @@ class Solver:
             block_tmp = list(filter((0).__ne__, i))
             if len(block_tmp) != len(set(block_tmp)):
                 sys.exit("invalid sudoku: duplicates in block")
+
+    def solve_0(self, sudoku):
+
 
 sudoku = [[1,2,3,4,5,6,7,8,9],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2],[4,5,6,7,8,9,1,2,3],[5,6,7,8,9,1,2,3,4],[6,7,8,9,1,2,3,4,5],[7,8,9,1,2,3,4,5,6],[8,9,1,2,3,4,5,6,7],[9,1,2,3,4,5,6,7,8]]
 solver = Solver(sudoku)
