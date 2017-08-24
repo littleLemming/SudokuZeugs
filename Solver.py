1f2129baf5cf2b8f2639b0eba0b7f1d7d9eb395b
littleLemming <lemming0061@gmail.com>
2017-08-24 17:40:52 +0200

    removeing old stuff - starting over

--- Solver.py
+++ Solver.py
@@ -1,48 +1,6 @@
-import sys
 
-class Solver:
-
-    # solves regular 9x9 sudokus
-    # input as array-array, for empty spaces 0
-
-    def __init__(self, sudoku):
-        self.valid(sudoku)
-        self.solutions = []
-
-    def finished_0(self, sudoku):
-        for row in sudoku:
-            row_tmp = list(filter((0).__ne__, sudoku[i]))
-                if len(row_tmp) != len(set(row_tmp)):
-                    return False
-        return True
-
-    def valid_0(self, sudoku):
-        if len(sudoku) != 9:
-            sys.exit("invalid sudoku: invalid size of sudoku")
-        for i in range (0,9):
-            # TODO: check if only 0-9 contained
-            if len(sudoku[i]) != 9:
-                sys.exit("invalid sudoku: invalid size of sudoku")
-            row_tmp = list(filter((0).__ne__, sudoku[i]))
-            if len(row_tmp) != len(set(row_tmp)):
-                sys.exit("invalid sudoku: duplicates in row")
-            column = [row[i] for row in sudoku]
-            column_tmp = list(filter((0).__ne__, column))
-            if len(column_tmp) != len(set(column_tmp)):
-                sys.exit("invalid sudoku: duplicates in column")
-        blocks = [[],[],[],[],[],[],[],[],[]]
-        for i in range(0,3): # row
-            for j in range(0,3): # column
-                blocks[i*3+j].extend(sudoku[i*3][j*3:j*3+3])
-                blocks[i*3+j].extend(sudoku[i*3+1][j*3:j*3+3])
-                blocks[i*3+j].extend(sudoku[i*3+2][j*3:j*3+3])
-        for i in blocks:
-            block_tmp = list(filter((0).__ne__, i))
-            if len(block_tmp) != len(set(block_tmp)):
-                sys.exit("invalid sudoku: duplicates in block")
-
-    def solve_0(self, sudoku):
-
-
-sudoku = [[1,2,3,4,5,6,7,8,9],[2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2],[4,5,6,7,8,9,1,2,3],[5,6,7,8,9,1,2,3,4],[6,7,8,9,1,2,3,4,5],[7,8,9,1,2,3,4,5,6],[8,9,1,2,3,4,5,6,7],[9,1,2,3,4,5,6,7,8]]
-solver = Solver(sudoku)
+# solving a simple 9x9 sudoku
+def solve_9x9(sudoku):
+    # find 0 - if no more can be found return sudoku
+    # make list of numbers that could be filled in - if no numbers can be filled in, return 
+    # call for every number solve_9x9 with copy of sudoku with one of those numbers
